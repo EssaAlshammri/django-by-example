4f41d5e51411f23e4bd0575edd3acf49591cc4a8
EssaAlshammri <essa.alshammri@ymail.com>
2017-03-21 00:19:06 +0300

    Creating custom template filters

--- blog/mysite/blog/templatetags/blog_tags.py
+++ blog/mysite/blog/templatetags/blog_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
 
+import markdown
 from blog.models import Post
 
 register = template.Library()
@@ -18,3 +20,8 @@ def show_latest_posts(count=5):
 @register.assignment_tag
 def get_most_commented_posts(count=5):
     return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
