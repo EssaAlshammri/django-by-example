015ccf06754da1bf84012815e8c7145682961c88
EssaAlshammri <essa.alshammri@ymail.com>
2017-03-20 22:03:25 +0300

    Adding tagging functionality

--- blog/mysite/blog/models.py
+++ blog/mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -25,6 +26,7 @@ class Post(models.Model):
 
     objects = models.Manager() # The default manager.
     published = PublishedManager() # The Dahl-specific manager.
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
--- blog/mysite/blog/views.py
+++ blog/mysite/blog/views.py
@@ -4,9 +4,14 @@ from django.views.generic import ListView
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
-def post_list(request, category=None):
+def post_list(request, category=None, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -18,7 +23,8 @@ def post_list(request, category=None):
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'page': page,
-                                                   'posts': posts})
+                                                   'posts': posts,
+                                                   'tag': tag})
 
 
 class PostListView(ListView):
