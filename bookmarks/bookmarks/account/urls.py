bcefa4fa36bb26d43ab89c8437127eb5cc76356d
EssaAlshammri <essa.alshammri@ymail.com>
2017-03-23 04:16:08 +0300

    Change password views

--- bookmarks/bookmarks/account/urls.py
+++ bookmarks/bookmarks/account/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout, logout_then_login
+from django.contrib.auth.views import (login, logout, logout_then_login,
+                                       password_change, password_change_done)
 
 from . import views
 
@@ -7,7 +8,15 @@ urlpatterns = [
     # url(r'^login/$', views.user_login, name='login'),
 
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
+
+    # login and logout urls
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
+
+    # password change urls
+    url(r'^password-change/$',
+        password_change, name='password_change'),
+    url(r'^password-change/done/$',
+        password_change_done, name='password_change_done'),
 ]
