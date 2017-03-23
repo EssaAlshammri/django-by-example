ac5b885dabcc57d819524952d20c4a68acaff9df
EssaAlshammri <essa.alshammri@ymail.com>
2017-03-23 04:17:54 +0300

    auto pep8

--- bookmarks/bookmarks/account/views.py
+++ bookmarks/bookmarks/account/views.py
@@ -11,7 +11,8 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
+            user = authenticate(
+                username=cd['username'], password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
