348d4003a18487f904c070513daa37f4a9c3f527
EssaAlshammri <essa.alshammri@ymail.com>
2017-03-23 03:09:18 +0300

    Creating a log-in view

--- bookmarks/bookmarks/account/views.py
+++ bookmarks/bookmarks/account/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('authenticated seccessfully')
+                else:
+                    return HttpResponse('Inactive account!')
+            else:
+                return HttpResponse('Invalid Login!')
+
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
