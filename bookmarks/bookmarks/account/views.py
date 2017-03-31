39d6a6c35b7049c77e7f10e6007503b2ad40b2b5
EssaAlshammri <essa.alshammri@ymail.com>
2017-03-31 18:03:36 +0300

    Building an AJAX view to follow users

--- bookmarks/bookmarks/account/views.py
+++ bookmarks/bookmarks/account/views.py
@@ -2,12 +2,15 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
+
+from common.decorators import ajax_required
 
 from .forms import (LoginForm, ProfileEditForm, UserEditForm,
                     UserRegistrationForm)
-from .models import Profile
+from .models import Contact, Profile
 
 
 def user_login(request):
@@ -78,3 +81,24 @@ def user_list(request):
 def user_detail(request, username):
     user = get_object_or_404(User, username=username, is_active=True)
     return render(request, 'account/user/detail.html', {'section': 'people', 'user': user})
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user, user_to=user)
+            else:
+                Contact.objects.filter(
+                    user_from=request.user, user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'ko'})
+    return JsonResponse({'status': 'ko'})
