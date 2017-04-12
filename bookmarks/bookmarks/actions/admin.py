f5073fd1fd68cf3c9e4e0925b6f283f6df9b432f
EssaAlshammri <essa.alshammri@ymail.com>
2017-04-12 03:02:21 +0300

    Adding generic relations to Action model

--- bookmarks/bookmarks/actions/admin.py
+++ bookmarks/bookmarks/actions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Action
+
+
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'verb', 'target', 'created']
+    list_filter = ['created']
+    search_fields = ['verb']
+
+admin.site.register(Action, ActionAdmin)
--- bookmarks/bookmarks/actions/models.py
+++ bookmarks/bookmarks/actions/models.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
 class Action(models.Model):
     user = models.ForeignKey(User, related_name='actions', db_index=True)
     verb = models.CharField(max_length=255)
+    target_ct = models.ForeignKey(
+        ContentType, null=True, blank=True, related_name='target_obj')
+    target_id = models.PositiveIntegerField(
+        null=True, blank=True, db_index=True)
+    target = GenericForeignKey('target_ct', 'target_id')
     created = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
