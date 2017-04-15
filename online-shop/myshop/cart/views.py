61e24b4f1381a4ef06f6e42f3a6fa56828329a6c
EssaAlshammri <essa.alshammri@ymail.com>
2017-04-15 09:47:15 +0300

    Adding items to the cart

--- online-shop/myshop/cart/views.py
+++ online-shop/myshop/cart/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from shop.models import Product
+
+from .cart import Cart
+from .forms import CartAddProductForm
+
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product, cd['quantity'], cd['update'])
+    return redirect('cart:cart_detail')
+
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
