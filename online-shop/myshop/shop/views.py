c558a80d57edae2a589fd6d13420f2937921150e
EssaAlshammri <essa.alshammri@ymail.com>
2017-04-15 10:00:39 +0300

    Adding products to the cart

--- online-shop/myshop/shop/views.py
+++ online-shop/myshop/shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 
+from cart.forms import CartAddProductForm
+
 from .models import Category, Product
 
 
@@ -19,4 +21,8 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product': product})
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {
+        'product': product,
+        'cart_product_form': cart_product_form
+        })
