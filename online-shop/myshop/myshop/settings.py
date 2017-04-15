540cad276dda08e6a086fb2793deb36fc31641ce
EssaAlshammri <essa.alshammri@ymail.com>
2017-04-15 08:54:33 +0300

    creating cart app

--- online-shop/myshop/myshop/settings.py
+++ online-shop/myshop/myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
