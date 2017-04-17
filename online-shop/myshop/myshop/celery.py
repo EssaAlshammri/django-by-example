c3382c6971614f5174e79ee90668f3b1ac0f75f7
EssaAlshammri <essa.alshammri@ymail.com>
2017-04-17 15:38:02 +0300

    Adding asynchronous tasks to the application

--- online-shop/myshop/myshop/celery.py
+++ online-shop/myshop/myshop/celery.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
 
-app = Celery('myshop')
+app = Celery('myshop',broker = 'amqp://guest:guest@172.17.0.2:5672//', backend = 'amqp://')
 
-app.confing_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
--- online-shop/myshop/orders/views.py
+++ online-shop/myshop/orders/views.py
@@ -4,6 +4,7 @@ from cart.cart import Cart
 
 from .forms import OrderCreateForm
 from .models import OrderItem
+from .tasks import order_created
 
 
 def order_create(request):
@@ -18,6 +19,7 @@ def order_create(request):
                                          price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
+            order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order})
     else:
         form = OrderCreateForm()
