09a26ca8cfca779273c97766d2045d01acb7b1d2
EssaAlshammri <essa.alshammri@ymail.com>
2017-04-21 15:23:27 +0300

    Exporting orders to CSV files

--- online-shop/myshop/orders/admin.py
+++ online-shop/myshop/orders/admin.py
@@ -1,6 +1,31 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from .models import Order, OrderItem
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
 
 
 class OrderItemInline(admin.TabularInline):
@@ -13,5 +38,6 @@ class OrderAdmin(admin.ModelAdmin):
                     'address', 'postal_code', 'city', 'paid', 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
 
 admin.site.register(Order, OrderAdmin)
