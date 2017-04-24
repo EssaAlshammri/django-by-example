669455897d748743e758ccd834083f134dec482e
EssaAlshammri <essa.alshammri@ymail.com>
2017-04-25 01:58:56 +0300

    Applying coupons to orders

--- online-shop/myshop/orders/models.py
+++ online-shop/myshop/orders/models.py
@@ -1,5 +1,9 @@
+from decimal import Decimal
+
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+from coupons.models import Coupon
 from shop.models import Product
 
 
@@ -13,6 +17,8 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
+    coupon = models.ForeignKey(Coupon, related_name='orders', null=True, blank=True)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     class Meta:
         ordering = ['-created']
@@ -21,7 +27,8 @@ class Order(models.Model):
         return 'order {}'.format(self.id)
 
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
+        total_cost = sum(item.get_cost() for item in self.items.all())
+        return total_cost - total_cost * (self.discount / Decimal('100'))
 
 
 class OrderItem(models.Model):
