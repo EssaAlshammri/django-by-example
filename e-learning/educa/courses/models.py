d2fd82c66387050095a67487f0c17f10988689df
EssaAlshammri <essa.alshammri@ymail.com>
2017-04-28 15:28:17 +0300

    Creating models for diverse content

--- e-learning/educa/courses/models.py
+++ e-learning/educa/courses/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -35,3 +37,10 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents')
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
