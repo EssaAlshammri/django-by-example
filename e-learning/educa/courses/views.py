b9457d7b84f947f4bc5877351b3b26cdd8b5a229
EssaAlshammri <essa.alshammri@ymail.com>
2017-04-30 18:09:35 +0300

    Using mixins for class-based views

--- e-learning/educa/courses/views.py
+++ e-learning/educa/courses/views.py
@@ -1,13 +1,47 @@
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .models import Course
 
 
-class ManageCourseListView(ListView):
-    model = Course
-    template_name = 'courses/manage/course/list.html'
+class OwnerMixin(object):
 
     def get_queryset(self):
-        qs = super(ManageCourseListView, self).get_queryset()
+        qs = super(OwnerMixin, self).get_queryset()
         return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin(object):
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(OwnerEditMixin, self).form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+    template_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+    success_url = reverse_lazy('manage_course_list')
