6dd834cee21a5a4118a90a4ab3b8387400b35871
EssaAlshammri <essa.alshammri@ymail.com>
2017-05-03 22:48:12 +0300

    Adding content to course modules

--- e-learning/educa/courses/urls.py
+++ e-learning/educa/courses/urls.py
@@ -13,4 +13,10 @@ urlpatterns = [
         views.CourseDeleteView.as_view(), name='course_delete'),
     url(r'^(?P<pk>\d+)/module/$',
         views.CourseModuleUpdateView.as_view(), name='course_module_update'),
+    url(r'^module/(?P<module_id>\d+)/content/(?P<model_name>\w+)/create/$',
+        views.ContentCreateUpdateView.as_view(), name='module_content_create'),
+    url(r'^module/(?P<module_id>\d+)/content/(?P<model_name>\w+)/(?P<id>\d+)/$',
+        views.ContentCreateUpdateView.as_view(), name='module_content_update'),
+    url(r'^content/(?P<id>\d+)/delete/$',
+        views.ContentDeleteView.as_view(), name='module_content_delete'),
 ]
--- e-learning/educa/courses/views.py
+++ e-learning/educa/courses/views.py
@@ -1,13 +1,15 @@
+from django.apps import apps
 from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         PermissionRequiredMixin)
 from django.core.urlresolvers import reverse_lazy
+from django.forms.models import modelform_factory
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import ListView
 from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .forms import ModuleFormSet
-from .models import Course
+from .models import Content, Course, Module
 
 
 class OwnerMixin(object):
@@ -73,3 +75,54 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
             formset.save()
             return redirect('manage_course_list')
         return self.render_to_response({'course': self.course, 'formset': formset})
+
+
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    model = None
+    module = None
+    obj = None
+    template_name = 'courses/manage/content/form.html'
+
+    def get_model(self, model_name):
+        if model_name in ['text', 'video', 'image', 'file']:
+            return apps.get_model('courses', model_name)
+        return None
+
+    def get_form(self, model, *args, **kwargs):
+        Form = modelform_factory(
+            model, exclude=['owner', 'order', 'created', 'updated'])
+        return Form(*args, **kwargs)
+
+    def dispatch(self, request, module_id, model_name, id=None):
+        self.module = get_object_or_404(
+            Module, id=module_id, course__owner=request.user)
+        self.model = self.get_model(model_name)
+        if id:
+            self.obj = get_object_or_404(self.model, id=id, owner=request.user)
+        return super(ContentCreateUpdateView, self).dispatch(request, module_id, model_name, id)
+
+    def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response({'form': form, 'object': self.obj})
+
+    def post(self, request, module_id, model_name, id=None):
+        form = self.get_form(
+            self.model, instance=self.obj, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+            if not id:
+                Content.objects.create(module=self.module, item=obj)
+            return redirect('module_content_list')
+        return self.render_to_response({'form': form, 'object': self.obj})
+
+
+class ContentDeleteView(View):
+
+    def post(self, request, id):
+        content = get_object_or_404(Content, id=id, module__course__owner=request.user)
+        module = content.module
+        content.item.delete()
+        content.delete()
+        return redirect('module_content_list', module.id)
