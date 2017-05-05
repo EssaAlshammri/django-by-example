25b9dee7a942a6c44ed6c8609a9cb01854c6b3d1
EssaAlshammri <essa.alshammri@ymail.com>
2017-05-05 17:07:27 +0300

    Reordering modules and contents

--- e-learning/educa/courses/urls.py
+++ e-learning/educa/courses/urls.py
@@ -21,4 +21,8 @@ urlpatterns = [
         views.ContentDeleteView.as_view(), name='module_content_delete'),
     url(r'^module/(?P<module_id>\d+)/$',
         views.ModuleContentListView.as_view(), name='module_content_list'),
+    url(r'^module/order/$',
+        views.ModuleOrderView.as_view(), name='module_order'),
+    url(r'^content/order/$',
+        views.ContentOrderView.as_view(), name='content_order'),
 ]
