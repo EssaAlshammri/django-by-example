4e7b179565d0392266021922806a3311a2c760f0
EssaAlshammri <essa.alshammri@ymail.com>
2017-05-08 23:32:15 +0300

    Adding additional actions to view sets

--- e-learning/educa/courses/api/urls.py
+++ e-learning/educa/courses/api/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
         views.SubjectListView.as_view(), name='subject_list'),
     url(r'^subjects/(?P<pk>\d+)/$',
         views.SubjectDetailView.as_view(), name='subject_detail'),
-    url(r'^courses/(?P<pk>\d+)/enroll/$',
-        views.CourseEnrollView.as_view(), name='course_enroll'),
+    # url(r'^courses/(?P<pk>\d+)/enroll/$',
+    #     views.CourseEnrollView.as_view(), name='course_enroll'),
     url(r'^', include(router.urls)),
 ]
--- e-learning/educa/courses/api/views.py
+++ e-learning/educa/courses/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import detail_route
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -19,16 +20,24 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
-class CourseEnrollView(APIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+# class CourseEnrollView(APIView):
+#     authentication_classes = [BasicAuthentication]
+#     permission_classes = [IsAuthenticated]
 
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
-        course.students.add(request.user)
-        return Response({'enrolled': True})
+#     def post(self, request, pk, format=None):
+#         course = get_object_or_404(Course, pk=pk)
+#         course.students.add(request.user)
+#         return Response({'enrolled': True})
 
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    @detail_route(methods=['post'],
+    authentication_classes=[BasicAuthentication],
+    permission_classes=[IsAuthenticated])
+    def enroll(self, request, pk, format=None):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
