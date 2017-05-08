39459f5de4c18c59e639148c1f7b389e7c818643
EssaAlshammri <essa.alshammri@ymail.com>
2017-05-08 22:29:14 +0300

    Installing Django Rest Framework

--- e-learning/educa/educa/settings.py
+++ e-learning/educa/educa/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'embed_video',
     'memcache_status',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -143,3 +144,9 @@ CACHES = {
 # CACHE_MIDDLEWARE_ALIAS = 'default'
 # CACHE_MIDDLEWARE_SECONDS = 60 * 15 # 15 minutes
 # CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
