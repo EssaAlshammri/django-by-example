82189d10255b3b6d4b4aa1d28c2a8d6d08dff24c
EssaAlshammri <essa.alshammri@ymail.com>
2017-05-08 22:57:13 +0300

    Creating nested serializers

--- e-learning/educa/courses/api/serializers.py
+++ e-learning/educa/courses/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from ..models import Subject
+from ..models import Course, Module, Subject
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -8,3 +8,19 @@ class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'title', 'slug']
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug',
+                  'overview', 'created', 'owner', 'modules']
