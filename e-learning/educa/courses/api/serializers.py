1f784294c23808ae14bae538ff03425b24a17f99
EssaAlshammri <essa.alshammri@ymail.com>
2017-05-08 22:36:34 +0300

    Defining serializers

---
+++ e-learning/educa/courses/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from ..models import Subject
+
+
+class SubjectSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Subject
+        fields = ['id', 'title', 'slug']
