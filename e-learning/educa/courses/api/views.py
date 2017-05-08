ab0df993c23cb72f3d10507d3882d7719fc1ef98
EssaAlshammri <essa.alshammri@ymail.com>
2017-05-08 23:03:02 +0300

    Building custom views

--- e-learning/educa/courses/api/views.py
+++ e-learning/educa/courses/api/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from ..models import Subject
+from ..models import Course, Subject
 from .serializers import SubjectSerializer
 
 
@@ -12,3 +15,11 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseEnrollView(APIView):
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
